6ee3337e68041c5311f84ae949d12ce02884adbd
Bruno Barlocco <brunodeether@gmail.com>
2019-04-25 22:06:21 +0200

    connect to device

--- android/src/main/java/com/reactlibrary/RNMobilePrinterSdkModule.java
+++ android/src/main/java/com/reactlibrary/RNMobilePrinterSdkModule.java
@@ -1,6 +1,7 @@
 
 package com.reactlibrary;
 import com.reactlibrary.models.Device;
+import com.reactlibrary.service.BluetoothService;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -13,17 +14,24 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.widget.Toast;
 import java.util.Set;
+import android.os.Handler;
 
 public class RNMobilePrinterSdkModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
   private BluetoothAdapter mBluetoothAdapter;
+  private BluetoothService bluetoothService;
   public RNMobilePrinterSdkModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
-    
+    this.bluetoothService = new BluetoothService(getReactApplicationContext(), new Handler());
   }
 
+  @ReactMethod 
+  public void connectToDevice(String address,Promise promise){
+    BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+    this.bluetoothService.connect(device);
+  }
   @ReactMethod
   public void startConnection(final Promise promise){
     this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
