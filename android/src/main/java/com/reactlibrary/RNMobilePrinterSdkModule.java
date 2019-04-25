70bc8bc0dd40d06495b75fb524e578f62314f04c
Bruno Barlocco <brunodeether@gmail.com>
2019-04-25 18:39:15 +0200

    test Toast

--- android/src/main/java/com/reactlibrary/RNMobilePrinterSdkModule.java
+++ android/src/main/java/com/reactlibrary/RNMobilePrinterSdkModule.java
@@ -5,16 +5,31 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.widget.Toast;
 
 public class RNMobilePrinterSdkModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
-
+  private BluetoothAdapter mBluetoothAdapter;
   public RNMobilePrinterSdkModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    
   }
 
+  @ReactMethod
+  public void startConnection(final Promise promise){
+    this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    if (mBluetoothAdapter == null) {
+			Toast.makeText(this, "Bluetooth is not available",
+					Toast.LENGTH_LONG).show();
+		}else{
+      Toast.makeText(this, "Adapter granted",
+					Toast.LENGTH_LONG).show();
+    }
+  }
   @ReactMethod
   public void showMessage(final Promise promise) {
       promise.resolve("ASD");
